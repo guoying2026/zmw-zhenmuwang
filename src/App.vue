4906e2ea690c5eb9c3e670a7b447766f3b34d01f
joytou <1540294142@qq.com>
2023-02-14 16:21:46 +0800

    添加导航栏

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,17 @@
 <template>
+  <el-affix>
+    <el-row justify="space-between" class="navbar" align="middle">
+      <el-col :span="3" :push="1">
+        <span class="text-extra-large font-600 mr-3">zmw</span>
+      </el-col>
+      <el-col :span="16">
+        <SearchBar />
+      </el-col>
+      <el-col :span="3" :pull="1" style="display: flex;flex-direction: row-reverse;">
+        <UserLogin />
+      </el-col>
+    </el-row>
+  </el-affix>
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
@@ -14,4 +27,22 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import { useRouter } from 'vue-router'
+import './assets/main.css'
 </script>
+<style scoped>
+.navbar {
+  height: var(--navbar-height);
+  line-height: var(--navbar-line-height);
+  background-color: var(--navbar-bg-color);
+}
+.el-affix,
+.el-affix--fixed {
+  background-color: var(--navbar-bg-color);
+  color: var(--navbar-ft-color);
+}
+.search_hide {
+  color: var(--navbar-ft-color);
+  background-color: var(--navbar-bg-color);
+  border-color: var(--navbar-bg-color);
+}
+</style>
