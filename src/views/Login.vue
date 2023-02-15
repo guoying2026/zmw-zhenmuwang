7a56f78622d5866a81c1f0aa1ca1a811ad0bc833
guoying <2473548838@qq.com>
2023-02-15 13:53:53 +0800

    login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -60,9 +60,7 @@
       <el-input v-model.number="ruleForm.age" />
     </el-form-item>
     <el-form-item>
-      <el-button type="primary" @click="submitForm(ruleFormRef)"
-      >Submit</el-button
-      >
+      <el-button type="primary" @click="submitForm(ruleFormRef)">Submit</el-button>
       <el-button @click="resetForm(ruleFormRef)">Reset</el-button>
     </el-form-item>
     <el-form-item label="Password" prop="pass">
