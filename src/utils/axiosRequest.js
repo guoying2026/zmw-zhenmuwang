799d043012fc4834ac16f24798c0d15484bd7e3c
guoying <2473548838@qq.com>
2023-02-24 09:50:35 +0800

    post

--- src/utils/axiosRequest.js
+++ src/utils/axiosRequest.js
@@ -5,6 +5,4 @@ axios.defaults.baseURL = 'https://api.zhenmuwang.com/';
 const axiosRequest = axios.create();
 //在请求超时前，会等待2.5s
 axiosRequest.defaults.timeout = 2500;
-//post请求需要解决同源策略
-// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 export default axiosRequest
