47c112adb593423eda7b6dee9424f88c5df772a3
guoying <2473548838@qq.com>
2023-02-28 14:06:40 +0800

    我要评论组件

---
+++ src/components/AddComment.vue
@@ -0,0 +1,130 @@
+<template>
+  <el-button type="primary" style="margin-left: 16px" @click="drawer = true">
+    {{startText}}
+  </el-button>
+
+  <el-drawer v-model="drawer" direction="btt" size="50%" title="I am the title" :with-header="false">
+    <!--          抽屉里的添加评论开始-->
+    <el-input
+        v-model="textarea"
+        maxlength="200"
+        :placeholder="placeholderText"
+        show-word-limit
+        type="textarea"
+        :rows="5"
+    />
+    <el-upload class="margin-20-top" action="#" list-type="picture-card" :auto-upload="false">
+      <el-icon><Plus /></el-icon>
+
+      <template #file="{ file }">
+        <div>
+          <img class="el-upload-list__item-thumbnail" :src="file.url" alt="" />
+          <span class="el-upload-list__item-actions">
+          <span
+              class="el-upload-list__item-preview"
+              @click="handlePictureCardPreview(file)"
+          >
+            <el-icon><zoom-in /></el-icon>
+          </span>
+          <span
+              v-if="!disabled"
+              class="el-upload-list__item-delete"
+              @click="handleDownload(file)"
+          >
+            <el-icon><Download /></el-icon>
+          </span>
+          <span
+              v-if="!disabled"
+              class="el-upload-list__item-delete"
+              @click="handleRemove(file)"
+          >
+            <el-icon><Delete /></el-icon>
+          </span>
+        </span>
+        </div>
+      </template>
+    </el-upload>
+
+    <el-dialog v-model="dialogVisible">
+      <img w-full :src="dialogImageUrl" alt="Preview Image" />
+    </el-dialog>
+    <el-row>
+      <el-col :span="12" class="center_button margin-20-top">
+        <el-button type="" @click="drawer = true">
+          {{cancelText}}
+        </el-button>
+      </el-col>
+      <el-col :span="12" class="center_button margin-20-top">
+        <el-button type="primary" @click="drawer = true">
+          {{confirmText}}
+        </el-button>
+      </el-col>
+    </el-row>
+
+    <!--          弹出层的评论结束-->
+  </el-drawer>
+</template>
+<script>
+export default{
+  name: 'AddComment'
+}
+</script>
+<script setup>
+import { ref } from 'vue'
+//添加评论开始
+//父组件给该组件AddComment传递的值，就定义在defineProps,开始
+const props = defineProps({
+  startText:{
+    type: String,
+    default: '我要评论'
+  },
+  placeholderText:{
+    type: String,
+    default: '添加评论'
+  },
+  cancelText:{
+    type: String,
+    default: '取消评论'
+  },
+  confirmText:{
+    type: String,
+    default: '发布评论'
+  }
+})
+//父组件给该组件AddComment传递的值，就定义在defineProps,结束
+//抽屉开始
+const drawer = ref(false)
+//抽屉结束
+
+//输入评论开始
+const textarea = ref('')
+//输入评论结束
+
+//上传文件开始
+const dialogImageUrl = ref('')
+const dialogVisible = ref(false)
+const disabled = ref(false)
+
+const handleRemove = (file) => {
+  console.log(file)
+}
+
+const handlePictureCardPreview = (file) => {
+  dialogImageUrl.value = file.url
+  dialogVisible.value = true
+}
+
+const handleDownload = (file) => {
+  console.log(file)
+}
+//上传文件结束
+//添加评论结束
+</script>
+<style scoped>
+.center_button{
+  text-align: center;
+}
+.margin-20-top{
+  margin-top: 20px;
+}
+</style>
--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,9 +1,133 @@
 <template>
-  <el-container>
-    <el-main>Main</el-main>
+  <div class="padding-10">
+  <el-container direction="vertical" :gutter="24">
+    <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
+      <el-tab-pane label="公司首页" name="first">
+<!--        公司首页的精选商品开始-->
+        <el-descriptions title="精选商品">
+          <template #extra>
+            <el-button type="" text>
+              查看全部<el-icon class="el-icon--right"><ArrowRight /></el-icon>
+            </el-button>
+          </template>
+        </el-descriptions>
+        <el-row :gutter="20">
+          <el-col
+              v-for="(o, index) in 8"
+              :key="o"
+              :span="24"
+              :md="index <= 8 ? {span:12} :{span:24}"
+          >
+            <el-card>
+              <img
+                  src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+                  class="image"
+              />
+              <div style="padding: 14px">
+                <span>Yummy hamburger</span>
+                <div class="bottom">
+                  <time class="time">{{ currentDate }}</time>
+                  <el-button text class="button">Operating</el-button>
+                </div>
+              </div>
+            </el-card>
+          </el-col>
+        </el-row>
+<!--        公司首页的精选商品结束-->
+<!--        公司首页的问大家开始-->
+        <el-descriptions title="问大家">
+          <template #extra>
+            <el-button type="" text>
+              查看全部<el-icon class="el-icon--right"><ArrowRight /></el-icon>
+            </el-button>
+          </template>
+        </el-descriptions>
+        <el-row :gutter="20">
+
+        </el-row>
+<!--        公司首页的问大家结束-->
+<!--        公司首页的大众评论开始-->
+        <el-descriptions title="大众评论">
+          <template #extra>
+            <el-button type="" text>
+              查看全部<el-icon class="el-icon--right"><ArrowRight /></el-icon>
+            </el-button>
+          </template>
+        </el-descriptions>
+        <AddComment
+            :placeholder-text="placeholderText"
+            :start-text="startText"
+            :cancel-text="cancelText"
+            :confirm-text="confirmText"
+        ></AddComment>
+<!--        公司首页的大众评论结束-->
+      </el-tab-pane>
+      <el-tab-pane label="商品" name="second">
+      </el-tab-pane>
+      <el-tab-pane label="问大家" name="third">问大家</el-tab-pane>
+      <el-tab-pane label="大众评论" name="fourth">大众评论</el-tab-pane>
+    </el-tabs>
   </el-container>
+  </div>
 </template>
 <script setup>
+import { ref } from 'vue'
+//评论开始
+//引入点击评论组件
+import AddComment from "../components/AddComment.vue";
+//给添加评论组件传值开始
+const placeholderText = ref('请输入评论');
+const startText = ref('我要评价');
+const cancelText = ref('取消评论');
+const confirmText = ref('发布评论');
+//给添加评论组件传值结束
+//评论结束
+//商品里面显示时间开始
+const currentDate = ref(new Date())
+//商品里面显示时间结束
+
+//导航栏切换开始
+const activeName = ref('first')
+const handleClick = (tab, event) => {
+  console.log(tab, event)
+}
+//导航栏切换结束
+
+const text = ref('')
 </script>
 <style scoped>
+.el-col{
+  margin-bottom: 20px;
+}
+.padding-10{
+  padding: 0 10px 20px 10px;
+}
+.demo-tabs > .el-tabs__content {
+  padding: 32px;
+  color: #6b778c;
+  font-size: 32px;
+  font-weight: 600;
+}
+.time {
+  font-size: 12px;
+  color: #999;
+}
+
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.button {
+  padding: 0;
+  min-height: auto;
+}
+
+.image {
+  width: 100%;
+  display: block;
+}
 </style>
