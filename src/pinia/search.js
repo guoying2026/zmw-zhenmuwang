6fe932871eaed6b798d2a5e41a71247ada4b69cf
joytou <1540294142@qq.com>
2023-03-14 15:20:46 +0800

    优化移动端列表页，触底加载更多更改为Element无限滚动组件

--- src/pinia/search.js
+++ src/pinia/search.js
@@ -10,6 +10,9 @@ export const useSearchStore = defineStore('searchStore', {
       _pageSize: 10,
       _totalPage: 0,
       _totalSize: 0,
+      _isLoading: false,
+      _isSuccess: false,
+      _isFailed: false,
     }
   },
   persist: true,
@@ -20,6 +23,9 @@ export const useSearchStore = defineStore('searchStore', {
     pageSize: (state) => state._pageSize,
     totalPage: (state) => state._totalPage,
     totalSize: (state) => state._totalSize,
+    isLoading: (state) => state._isLoading,
+    isSuccess: (state) => state._isSuccess,
+    isFailed: (state) => state._isFailed,
   },
   actions: {
     /**
@@ -29,8 +35,12 @@ export const useSearchStore = defineStore('searchStore', {
      * @param {Number|string} [data.size]
      */
     search(data) {
+      this._isLoading = true
+      this._isSuccess = false
+      this._isFailed = false
       getSearchResultApi(data).then(res => {
         if (res.status != 200 || res.data.status != 1000) {
+          this._isFailed = true
           return false
         }
         // 存储搜索结果
@@ -40,6 +50,11 @@ export const useSearchStore = defineStore('searchStore', {
         this._pageSize = Number(res.data.data.page_size)
         this._totalPage = Number(res.data.data.total_page)
         this._totalSize = Number(res.data.data.total_size)
+        this._isSuccess = true
+      }).catch(() => {
+        this._isFailed = true
+      }).finally(() => {
+        this._isLoading = false
       })
     },
     nextPage() {
