fe7c58c677ade84d226497a3b5f5081562c88e59
joytou <1540294142@qq.com>
2023-03-29 20:38:08 +0800

    调整样式

--- src/components/GoodsRecommends.vue
+++ src/components/GoodsRecommends.vue
@@ -71,6 +71,12 @@ const props = defineProps({
   text-align: center;
   font-size: var(--el-font-size-base);
 }
+.el-link {
+  color: inherit;
+}
+.el-link:hover {
+  color: var(--el-color-info);
+}
 .recommend_goods-item-info-price {
   text-align: center;
   font-size: var(--el-font-size-base);
