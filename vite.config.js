2f2899c2d2fe442dc1b90ea606b84153b05446d7
joytou <1540294142@qq.com>
2023-04-02 16:46:08 +0800

    修复搜索结果页面顶部距离问题

--- vite.config.js
+++ vite.config.js
@@ -22,56 +22,68 @@ export default defineConfig({
         {
           name: 'vue',
           var: 'Vue',
+          // path: 'https://cdn.bootcdn.net/ajax/libs/vue/3.2.45/vue.global.min.js',
+          // path: 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.45/vue.global.min.js',
+          // path: 'https://lib.baomitu.com/vue/3.2.45/vue.global.min.js',
           path: 'https://cdn.jsdelivr.net/npm/vue@3.2.45/dist/vue.global.min.js',
         },
         {
           name: 'vue-router',
           var: 'VueRouter',
+          // path: 'https://cdn.bootcdn.net/ajax/libs/vue-router/4.1.6/vue-router.global.min.js',
+          // path: 'https://cdnjs.cloudflare.com/ajax/libs/vue-router/4.1.6/vue-router.global.min.js',
+          // path: 'https://lib.baomitu.com/vue-router/4.1.6/vue-router.global.min.js',
           path: 'https://cdn.jsdelivr.net/npm/vue-router@4.1.6/dist/vue-router.global.min.js',
         },
         {
           name: 'element-plus',
           var: 'ElementPlus',
+          // path: 'https://cdn.bootcdn.net/ajax/libs/element-plus/2.2.29/index.full.min.js',
+          // path: 'https://cdnjs.cloudflare.com/ajax/libs/element-plus/2.2.29/index.full.min.js',
+          // path: 'https://lib.baomitu.com/element-plus/2.2.29/index.full.min.js',
           path: 'https://cdn.jsdelivr.net/npm/element-plus@2.2.29/dist/index.full.min.js',
+          // css: 'https://cdn.bootcdn.net/ajax/libs/element-plus/2.2.29/index.css',
+          // css: 'https://cdnjs.cloudflare.com/ajax/libs/element-plus/2.2.29/index.min.css',
+          // css: 'https://lib.baomitu.com/element-plus/2.2.29/index.min.css',
           css: 'https://cdn.jsdelivr.net/npm/element-plus@2.2.29/dist/index.css'
         },
         {
           name: '@element-plus/icons-vue',
           var: 'ElementPlusIconsVue',
+          // path: 'https://cdn.bootcdn.net/ajax/libs/element-plus-icons-vue/2.0.6/index.iife.min.js',
+          // path: 'https://cdnjs.cloudflare.com/ajax/libs/element-plus-icons-vue/2.0.6/index.iife.min.js',
+          // path: 'https://lib.baomitu.com/element-plus-icons-vue/2.0.6/index.iife.min.js',
           path: 'https://cdn.jsdelivr.net/npm/@element-plus/icons-vue@2.0.6/dist/index.iife.min.js',
         },
         {
           name: 'vue-demi',
           var: 'VueDemi',
-          // path: 'https://cdn.bootcdn.net/ajax/libs/vue-demi/0.13.1/index.iife.js'
+          // path: 'https://cdn.bootcdn.net/ajax/libs/vue-demi/0.13.11/index.iife.min.js',
+          // path: 'https://cdnjs.cloudflare.com/ajax/libs/vue-demi/0.13.11/index.iife.min.js',
+          // path: 'https://lib.baomitu.com/vue-demi/0.13.11/index.iife.min.js',
           path: 'https://cdn.jsdelivr.net/npm/vue-demi@0.13.11/lib/index.iife.min.js',
         },
         {
           name: 'pinia',
           var: 'Pinia',
-          // path: 'https://cdn.bootcdn.net/ajax/libs/pinia/2.0.30/pinia.iife.prod.min.js'
+          // path: 'https://cdn.bootcdn.net/ajax/libs/pinia/2.0.30/pinia.iife.min.js',
+          // path: 'https://cdnjs.cloudflare.com/ajax/libs/pinia/2.0.30/pinia.iife.min.js',
+          // path: 'https://lib.baomitu.com/pinia/2.0.30/pinia.iife.min.js',
           path: 'https://cdn.jsdelivr.net/npm/pinia@2.0.30/dist/pinia.iife.min.js',
         },
         {
           name: 'axios',
           var: 'axios',
-          path: 'https://cdn.jsdelivr.net/npm/axios@1.3.3/dist/axios.min.js'
+          // path: 'https://cdn.bootcdn.net/ajax/libs/axios/1.3.3/axios.min.js',
+          // path: 'https://cdnjs.cloudflare.com/ajax/libs/axios/1.3.3/axios.min.js',
+          // path: 'https://lib.baomitu.com/axios/1.3.3/axios.min.js',
+          path: 'https://cdn.jsdelivr.net/npm/axios@1.3.3/dist/axios.min.js',
         },
-        /* {
-          name: 'jsonwebtoken',
-          var: 'Jsonwebtoken',
-          path: 'https://cdn.jsdelivr.net/npm/jsonwebtoken@9.0.0/index.min.js',
-        }, */
         {
           name: 'mockjs',
           var: 'Mock',
           path: 'https://cdn.jsdelivr.net/npm/mockjs@1.1.0/dist/mock-min.min.js',
         },
-        /* {
-          name: 'qrcode.vue',
-          var: 'QrcodeVue',
-          path: 'https://cdn.jsdelivr.net/npm/qrcode.vue@3.3.4/dist/qrcode.vue.browser.min.js',
-        }, */
       ],
     }),
     // ...
