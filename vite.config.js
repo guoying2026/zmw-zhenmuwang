e4a04713766bb096b81a55bff0db5e40410b904b
joytou <1540294142@qq.com>
2023-04-02 15:34:51 +0800

    生产环境改用cdn引用第三方库

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import path from 'path'
 import viteCompression from 'vite-plugin-compression'
+import { Plugin as importToCDN } from 'vite-plugin-cdn-import'
 
 export default defineConfig({
   // ...
@@ -16,6 +17,63 @@ export default defineConfig({
   base: './',
   plugins: [
     vue(),
+    importToCDN({
+      modules: [
+        {
+          name: 'vue',
+          var: 'Vue',
+          path: 'https://cdn.jsdelivr.net/npm/vue@3.2.45/dist/vue.global.min.js',
+        },
+        {
+          name: 'vue-router',
+          var: 'VueRouter',
+          path: 'https://cdn.jsdelivr.net/npm/vue-router@4.1.6/dist/vue-router.global.min.js',
+        },
+        {
+          name: 'element-plus',
+          var: 'ElementPlus',
+          path: 'https://cdn.jsdelivr.net/npm/element-plus@2.2.29/dist/index.full.min.js',
+          css: 'https://cdn.jsdelivr.net/npm/element-plus@2.2.29/dist/index.css'
+        },
+        {
+          name: '@element-plus/icons-vue',
+          var: 'ElementPlusIconsVue',
+          path: 'https://cdn.jsdelivr.net/npm/@element-plus/icons-vue@2.0.6/dist/index.iife.min.js',
+        },
+        {
+          name: 'vue-demi',
+          var: 'VueDemi',
+          // path: 'https://cdn.bootcdn.net/ajax/libs/vue-demi/0.13.1/index.iife.js'
+          path: 'https://cdn.jsdelivr.net/npm/vue-demi@0.13.11/lib/index.iife.min.js',
+        },
+        {
+          name: 'pinia',
+          var: 'Pinia',
+          // path: 'https://cdn.bootcdn.net/ajax/libs/pinia/2.0.30/pinia.iife.prod.min.js'
+          path: 'https://cdn.jsdelivr.net/npm/pinia@2.0.30/dist/pinia.iife.min.js',
+        },
+        {
+          name: 'axios',
+          var: 'axios',
+          path: 'https://cdn.jsdelivr.net/npm/axios@1.3.3/dist/axios.min.js'
+        },
+        /* {
+          name: 'jsonwebtoken',
+          var: 'Jsonwebtoken',
+          path: 'https://cdn.jsdelivr.net/npm/jsonwebtoken@9.0.0/index.min.js',
+        }, */
+        {
+          name: 'mockjs',
+          var: 'Mock',
+          path: 'https://cdn.jsdelivr.net/npm/mockjs@1.1.0/dist/mock-min.min.js',
+        },
+        /* {
+          name: 'qrcode.vue',
+          var: 'QrcodeVue',
+          path: 'https://cdn.jsdelivr.net/npm/qrcode.vue@3.3.4/dist/qrcode.vue.browser.min.js',
+        }, */
+      ],
+    }),
     // ...
     AutoImport({
       resolvers: [ElementPlusResolver()],
@@ -36,13 +94,13 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks (id) {
-          if (id.includes('node_modules')) {
+          /* if (id.includes('node_modules')) {
             return id
               .toString()
               .split('node_modules/')[1]
               .split('/')[0]
               .toString();
-          }/*  else if (id.includes('/src/views/')) {
+          } else if (id.includes('/src/views/')) {
             return id
               .toString()
               .split('/src/views/')[1]
