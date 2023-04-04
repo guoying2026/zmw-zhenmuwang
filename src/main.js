3cf8aa5f29c7e2e5dfefa85f8db378f4ef29f508
joytou <1540294142@qq.com>
2023-04-04 11:22:34 +0800

    完善组件导入

--- src/main.js
+++ src/main.js
@@ -54,6 +54,7 @@ import {
     ElIcon,
     ElPagination,
     ElImage,
+    ElBacktop,
 } from 'element-plus'
 
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
@@ -104,6 +105,7 @@ app.use(ElContainer)
     .use(ElIcon)
     .use(ElPagination)
     .use(ElImage)
+    .use(ElBacktop)
 
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
