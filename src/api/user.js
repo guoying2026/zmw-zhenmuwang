cdaf80a9f42c2ded8277fe71743c20fe9b3d6919
joytou <1540294142@qq.com>
2023-04-10 15:11:30 +0800

    修复登录接口报错问题

--- src/api/user.js
+++ src/api/user.js
@@ -5,13 +5,19 @@ export const sendSMSVerificationCodeApi = (data) => {
     return axiosRequest({
         url: '/Pc/User/apiSendSms',
         method: 'post',
-        data
+        data: data,
+        headers: {
+            "Content-Type": 'application/x-www-form-urlencoded'
+        }
     })
 }
 export const loginApi = (data) => {
     return axiosRequest({
         url: '/Pc/User/login',
         method: 'post',
-        data
+        data: data,
+        headers: {
+            "Content-Type": 'application/x-www-form-urlencoded'
+        }
     })
 }
