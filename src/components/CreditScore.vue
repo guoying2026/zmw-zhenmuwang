ed5fecb4fc792df5c07a35c152082a6ed775fad3
guoying <2473548838@qq.com>
2023-04-10 15:52:39 +0800

    css

--- src/components/CreditScore.vue
+++ src/components/CreditScore.vue
@@ -2,6 +2,7 @@
   <div
       class="fluctuate margin-20-top"
       :style="`height: ${height}px;width:${width}`"
+      :class="height === '200'?'height_important':''"
   >
     <span class="font-15-size">{{creditScoreText}} </span>
     <span :class="`font-${fontSize}-size`">{{creditScore}}</span>
@@ -12,12 +13,70 @@
   @supports (-webkit-appearance:none) and (display:flow-root) {
     .font-150-size { font-size: 80px !important; }
     .font-40-size{ font-size: 30px !important;}
+    .height_important{ height: 150px !important;}
   }
 }
-@media not all and (min-resolution:.001dpcm) { @supports (-webkit-appearance:none) {
+@media not all and (min-resolution:.001dpcm) {
+  @supports (-webkit-appearance:none) and (stroke-color:transparent) {
+    .font-150-size { font-size: 80px !important; }
+    .font-40-size{ font-size: 30px !important;}
+    .height_important{ height: 150px !important;}
+
+  }
+}
+@media not all and (min-resolution:.001dpcm) {
+  @supports (-webkit-appearance:none) {
+    .font-150-size { font-size: 80px !important; }
+    .font-40-size{ font-size: 30px !important;}
+    .height_important{ height: 150px !important;}
+
+  }
+}
+@media not all and (min-resolution:.001dpcm) {
+  @media {
+    .font-150-size { font-size: 80px !important; }
+    .font-40-size{ font-size: 30px !important;}
+    .height_important{ height: 150px !important;}
+
+  }
+}
+@media not all and (min-resolution:.001dpcm) {
+  @supports (-webkit-appearance:none) and (not (stroke-color:transparent)) {
   .font-150-size { font-size: 80px !important; }
   .font-40-size{ font-size: 30px !important;}
+    .height_important{ height: 150px !important;}
+
+  }
 }
+@supports (-webkit-hyphens:none) {
+  .font-150-size { font-size: 80px !important; }
+  .font-40-size{ font-size: 30px !important;}
+  .height_important{ height: 150px !important;}
+
+}
+@supports (-webkit-marquee-repetition:infinite) and (object-fit:fill) {
+  .font-150-size { font-size: 80px !important; }
+  .font-40-size{ font-size: 30px !important;}
+  .height_important{ height: 150px !important;}
+
+}
+@supports (overflow:-webkit-marquee) and (justify-content:inherit) {
+  .font-150-size { font-size: 80px !important; }
+  .font-40-size{ font-size: 30px !important;}
+  .height_important{ height: 150px !important;}
+
+}
+@media screen and (min-color-index:0) and(-webkit-min-device-pixel-ratio:0) {
+  @media {
+    .font-150-size { font-size: 80px !important; }
+    .font-40-size{ font-size: 30px !important;}
+    .height_important{ height: 150px !important;}
+
+  } }
+@media only screen and (-webkit-min-device-pixel-ratio:1) { ::i-block-chrome, .font-150-size { font-size: 80px !important; }
+  .font-40-size{ font-size: 30px !important;}
+  .height_important{ height: 150px !important;}
+
 }
 .fluctuate span {
   display: inline-block;
