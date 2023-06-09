4ed4777465f085a437a0b581eecc4ae7e4e99ebc
joytou <1540294142@qq.com>
2023-06-09 15:10:34 +0800

    修复详情页在访问shop_info信息为空的商家时，直接报错的问题

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -423,7 +423,7 @@
           </p>
           <p>
             <span class="row_left">登记机关：</span>
-            <span class="row_right">{{ shop_info.regist_author ? shop_info.regist_author : '-' }}</span>
+            <span class="row_right">{{ shop_info && shop_info.regist_author ? shop_info.regist_author : '-' }}</span>
           </p>
           <!-- <p>
             <span class="row_left">人员规模：</span>
@@ -626,7 +626,7 @@
           <div class="roadmap__timeline special" style="--roadmap-start: 8; --roadmap-end: 12;">
             <div>
               <text>登记机关：</text>
-              <strong>{{ shop_info.regist_author ? shop_info.regist_author : '-' }}</strong></div>
+              <strong>{{ shop_info && shop_info.regist_author ? shop_info.regist_author : '-' }}</strong></div>
           </div>
         </div>
         <div class="roadmap__row">
